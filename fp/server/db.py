17e45c8dce07a48e08866c1c94b802f5a6253161
djohanp <djohan.prabowo1927@gmail.com>
2017-11-09 07:49:29 +0700

    Database mysql awal

---
+++ fp/server/db.py
@@ -0,0 +1,6 @@
+from __future__ import print_function
+from datetime import date, datetime, timedelta
+import mysql.connector
+#sudo dpkg -i mysql-connector-python_2.0.5-1ubuntu16.04_all.deb install mysql
+
+
